8c9206055b620e3796e8ef0d3539d78f4560cf68
TSIANOY Karl Elvestino Dorelin <elvestinodorelin@gmail.com>
2025-09-08 14:45:54 +0300

    change SPA

--- src/app/components/LetsTalk.tsx
+++ src/app/components/LetsTalk.tsx
@@ -4,14 +4,20 @@ import { MessageCircle } from "lucide-react";
 import { Button } from "./ui/button";
 
 export default function LetsTalk() {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault(); // éviter le jump brutal
+    const target = document.querySelector("#contact");
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <Button
-      asChild
-      className="bg-transparent text-[#7cf03d] border-2 border-[#7cf03d] shadow-none hover:bg-[#7cf03d] hover:text-[#1f242d] hover:shadow-[0_0_10px_#7cf03d] rounded-full px-6 py-3 font-semibold"
+      onClick={handleClick}
+      className="bg-transparent text-[#7cf03d] border-2 border-[#7cf03d] shadow-none hover:bg-[#7cf03d] cursor-pointer hover:text-[#1f242d] hover:shadow-[0_0_10px_#7cf03d] rounded-full px-6 py-3 font-semibold flex items-center justify-center"
     >
-      <a href="#contact">
-        Let&apos;s Talk <MessageCircle className="ml-2 h-5 w-5" />
-      </a>
+      Let&apos;s Talk <MessageCircle className="ml-2 h-5 w-5" />
     </Button>
   );
 }
